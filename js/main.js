5a9025ed0c3e98b666ec63f9d9b63db5daa4cb7d
Josue Muñoz Avila <sjosue5082002@gmail.com>
2021-06-17 13:01:37 -0500

    Acabado

--- js/main.js
+++ js/main.js
@@ -1,139 +1,289 @@
-$(document).ready(function () {
+$(document).ready(function(){
 
-/////////BANNER///////////// 
-    //encontar a banner
-    var banner ={
-        padre: $('#banner'),
-        numeroSlides: $('#banner').children('.slide').length,
-        posicion:1
-    }
-    //aplicar al primer slide de banner los estilos css de left
-    banner.padre.children('.slide').first().css({
-        'left':0
-    });
-    //Calcular alto de baner slide banner
-    var altoBanner = function () {
-        var alto = banner.padre.children('.slide').outerHeight();
+	// Objeto del Banner
+	var banner = {
+		padre: $('#banner'),
+		numeroSlides: $('#banner').children('.slide').length,
+		posicion: 1
+	};
 
-        banner.padre.css({
-            'height': alto + 'px'
-        });
-    }
-    altoBanner(); 
+	// Objeto del Slider de Info
+	var info = {
+		padre: $('#info'),
+		numeroSlides: $('#info').children('.slide').length,
+		posicion: 1
+	}
 
-    $(window).resize(function () {
-        altoBanner();
-    });
+	// Establecemos que el primer slide aparecera desplazado
+	banner.padre.children('.slide').first().css({
+		'left': 0
+	});
 
-    /*-----BANNER-----*/
-    //boton siguiente
-    $('#banner_next').on('click', function (e) {
-        e.preventDefault();
+	// Establecemos que el primer slide aparecera desplazado
+	info.padre.children('.slide').first().css({
+		'left': 0
+	});	
+	
+	// Funcion para calcular el alto que tendran los contenedores padre
+	var altoBanner = function(){
+		var alto = banner.padre.children('.slide').outerHeight();
+		banner.padre.css({
+			'height': alto + 'px'
+		});
+	}
 
-        if (banner.posicion < banner.numeroSlides) {
-            //nos aseguramos que las demas slides empiecen desde la derecha
-            banner.padre.children().not('.active').css({
-                'left':'100%'
-            });
-            //quitaos la clave active y se la ponemos al siguiente elemento, y lo animamos
-          $('#banner .active').removeClass('active').next().addClass('active').animate({
-            'left':'0'
-            });  
-            //Animamos el slide anterior para que se desplace hacia la izquierda
-            $('#banner .active').prev().animate({
-                'left':'-100%'
-            });
+	var altoInfo = function(){
+		var alto = info.padre.children('.active').outerHeight();
+		info.padre.animate({
+			'height': alto + 'px'
+		});
+	}
 
-            banner.posicion = banner.posicion + 1;
-        }else{
-            //hacemos que el slide activo (el ultimo), se anime hacia la derecha
-            $('#banner .active').animate({
-                'left':'-100%'
-            });
-            //seleccionamos todos lo slides que no tengan la clase .active
-            //y los posicionamos hacia la derecha
-            banner.padre.children().not('.active').css({
-                'left':'100%'
-            }); 
-            //elimiamos la clase active y se la ponemos al siguiete elemento
-            //despues lo animanos
-            $('#banner .active').removeClass('active');
-            banner.padre.children('.slide').first().addClass('active').animate({
-                'left':'0'
-            }); 
-            //reseteamos la posicion a 1
-            banner.posicion = 1;
-        }
-        
-    });
+	// Establecemos que el #contenedor tenga el 100% del alto de la pagina. 
+	// Para despues centrarlo verticalente con flexbox.
+	var altoContenedor = function(){
+		var altoVentana = $(window).height();
 
-    // boton anterior
-    $('#banner_prev').on('click', function (e) {
-        e.preventDefault();
+		if (altoVentana <= $('.contenedor').outerHeight() + 200) {
+			$('#contenedor').css({'height': ''});
+		} else {
+			$('#contenedor').css({'height': altoVentana + 'px'});
+		}
+	}
 
-        if (banner.posicion > 1) {
-            //nos aseguramos que las demas slides empiecen desde la izquierda
-            banner.padre.children().not('.active').css({
-                'left':'-100%'
-            });
+	// Ejecutamos las funciones para calcular los altos.
+	altoBanner();
+	altoContenedor();
+	altoInfo();
 
-            //Animamos el slide anterior para que se desplace hacia la izquierda
-            $('#banner .active').animate({
-                'left':'100%'
-            });
+	// Si cambiamos el tamaño de la pantalla se
+	// ejecuta esta funcion para saber el nuevo
+	// tamaño del elemento padre
+	$(window).resize(function(){
+		altoBanner();
+		altoContenedor();
+	});
 
-            //quitaos la clave active y se la ponemos al anterior elemento, y lo animamos
-            $('#banner .active').removeClass('active').prev().addClass('active').animate({
-                'left':'0'
-            });  
-            
-            banner.posicion = banner.posicion - 1;
+	// Agregamos un puntito por cada slide que tengamos
+	$('#info').children('.slide').each(function(){
+		$('#botones').append('<span>');
+	});
 
-            
-        }else{
-            //seleccionamos todos lo slides que no tengan la clase .active
-            //y los posicionamos hacia la izquierda
-            banner.padre.children().not('.active').css({
-                'left':'-100%'
-            });
+	// Declaramos que el primer elemento inicie con su clase active
+	$('#botones').children('span').first().addClass('active');
 
-            //hacemos que el slide activo (el primero), se anime hacia la izquierda
-            $('#banner .active').animate({
-                'left':'100%'
-            });
-             
-            //elimiamos la clase active y se la ponemos al anterior elemento
-            //despues lo animanos
-            $('#banner .active').removeClass('active');
-            banner.padre.children('.slide').last().addClass('active').animate({
-                'left':'0'
-            }); 
-            //reseteamos la posicion al numero de slides
-            banner.posicion = banner.numeroSlides;
-        }   
-        
-    });
+// ---------------------------------------
+// ----- Banner
+// ---------------------------------------
 
-/////////////INFO/////////////////// 
-    //encontar a info 
-    var info ={
-        padre: $('#info'),
-        numeroSlides: $('#info').children('.slide').length,
-        posicion:1
-    }
-    
-    //aplicar al primer slide de info los estilos css de left
-    info.padre.children('.slide').first().css({
-        'left':0
-    });
+	// Boton Siguiente
 
-    //Calcular alto de slide info
-    var altoinfo = function () {
-        var alto = info.padre.children('.slide').outerHeight();
+	$('#banner-next').on('click', function(e){
+		e.preventDefault();
 
-        info.padre.css({
-            'height': alto + 'px'
-        });
-    }
-    altoinfo();
+		if (banner.posicion < banner.numeroSlides){
+			// Nos aseguramos de que las demas slides empiecen desde la derecha.
+			banner.padre.children().not('.active').css({
+				'left': '100%'
+			});
+
+			// Quitamos la clase active y se la ponemos al siguiente elemento.Y lo animamos
+			$('#banner .active').removeClass('active').next().addClass('active').animate({
+				'left': 0
+			});
+
+			// Animamos el slide anterior para que se deslaza hacia la izquierda
+			$('#banner .active').prev().animate({
+				'left': '-100%'
+			});
+
+			banner.posicion = banner.posicion + 1;
+		} else {
+			// Hacemos que el slide activo (es decir el ultimo), se anime hacia la derecha
+			$('#banner .active').animate({
+				'left': '-100%'
+			});
+
+			// Seleccionamos todos los slides que no tengan la clase .active
+			// y los posicionamos a la derecha
+			banner.padre.children().not('.active').css({
+				'left': '100%'
+			});
+
+			// Eliminamos la clase active y se la ponemos al primer elemento.
+			// Despues lo animamos.
+			$('#banner .active').removeClass('active');
+			banner.padre.children().first().addClass('active').animate({
+				'left': 0
+			});
+
+			// Reseteamos la posicion a 1
+			banner.posicion = 1;
+		}
+	});
+
+	// Boton Anterior
+		$('#banner-prev').on('click', function(e){
+			e.preventDefault();
+
+			if (banner.posicion > 1){
+
+				// Nos asegramos de todos los elementos hijos (que no sean) .active
+				// se posicionen a la izquierda
+				banner.padre.children().not('.active').css({
+					'left': '-100%'
+				});
+
+				// Deslpazamos el slide activo de izquierda a derecha
+				$('#banner .active').animate({
+					'left': '100%'
+				});
+
+				// Eliminamos la clase active y se la ponemos al slide anterior.
+				// Y lo animamos
+				$('#banner .active').removeClass('active').prev().addClass('active').animate({
+					'left': 0
+				});
+
+				// Reiniciamos la posicion a 1
+				banner.posicion = banner.posicion - 1;
+			} else {
+
+				// Nos aseguramos de que los slides empiecen a la izquierda
+				banner.padre.children().not('.active').css({
+					'left': '-100%'
+				});
+
+				// Animamos el slide activo hacia la derecha
+				$('#banner .active').animate({
+					'left': '100%'
+				});
+
+				// Eliminamos la clase active y se la ponemos al primer elemento.
+				// Despues lo animamos.
+				$('#banner .active').removeClass('active');
+				banner.padre.children().last().addClass('active').animate({
+					'left': 0
+				});
+
+				// Reseteamos la posicion a 1
+				banner.posicion = banner.numeroSlides;
+			}
+
+		});
+
+// ---------------------------------------
+// ----- Slider Info
+// ---------------------------------------
+// Boton Siguiente
+
+	$('#info-next').on('click', function(e){
+		e.preventDefault();
+
+		if (info.posicion < info.numeroSlides){
+			// Nos aseguramos de que las demas slides empiecen desde la derecha.
+			info.padre.children().not('.active').css({
+				'left': '100%'
+			});
+
+			// Quitamos la clase active y se la ponemos al siguiente elemento.Y lo animamos
+			$('#info .active').removeClass('active').next().addClass('active').animate({
+				'left': 0
+			});
+
+			// Animamos el slide anterior para que se deslaza hacia la izquierda
+			$('#info .active').prev().animate({
+				'left': '-100%'
+			});
+
+			// Eliminamos la clase active y se la ponemos al siguiente elemento
+			$('.botones').children('.active').removeClass('active').next().addClass('active');
+				
+			// Incrementamos la posicion en 1
+			info.posicion = info.posicion + 1;
+		} else {
+			// Hacemos que el slide activo (es decir el ultimo), se anime hacia la derecha
+			$('#info .active').animate({
+				'left': '-100%'
+			});
+
+			// Seleccionamos todos los slides que no tengan la clase .active
+			// y los posicionamos a la derecha
+			info.padre.children().not('.active').css({
+				'left': '100%'
+			});
+
+			// Eliminamos la clase active y se la ponemos al primer elemento
+			// Despues lo animamos.
+			$('#info .active').removeClass('active');
+			info.padre.children().first().addClass('active').animate({
+				'left': 0
+			});
+
+			// Eliminamos la clase active y se la ponemos al primer elemento
+			$('.botones').children('.active').removeClass('active');
+			$('.botones').children('span').first().addClass('active');
+
+			// Reseteamos la posicion a 1
+			info.posicion = 1;
+		}
+
+		altoInfo();
+	});
+
+	// Boton Anterior
+		$('#info-prev').on('click', function(e){
+			e.preventDefault();
+
+			if (info.posicion > 1){
+
+				// Nos asegramos de todos los elementos hijos (que no sean) .active
+				// se posicionen a la izquierda
+				info.padre.children().not('.active').css({
+					'left': '-100%'
+				});
+
+				// Deslpazamos el slide activo de izquierda a derecha
+				$('#info .active').animate({
+					'left': '100%'
+				});
+
+				// Eliminamos la clase active y se la ponemos al slide anterior.
+				// Y lo animamos
+				$('#info .active').removeClass('active').prev().addClass('active').animate({
+					'left': 0
+				});
+
+				$('#botones').children('.active').removeClass('active').prev().addClass('active');
+
+				// Reiniciamos la posicion a 1
+				info.posicion = info.posicion - 1;
+			} else {
+
+				// Nos aseguramos de que los slides empiecen a la izquierda
+				info.padre.children().not('.active').css({
+					'left': '-100%'
+				});
+
+				// Animamos el slide activo hacia la derecha
+				$('#info .active').animate({
+					'left': '100%'
+				});
+
+				// Eliminamos la clase active y se la ponemos al primer elemento.
+				// Despues lo animamos.
+				$('#info .active').removeClass('active');
+				info.padre.children().last().addClass('active').animate({
+					'left': 0
+				});
+
+				$('#botones').children('.active').removeClass('active');
+				$('#botones').children('span').last().addClass('active');
+
+				// Reseteamos la posicion a 1
+				info.posicion = info.numeroSlides;
+			}
+
+			altoInfo();
+		});
 });
